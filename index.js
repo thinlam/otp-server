bded7e13bc2424b8699c50588910e48157233d74
thinjyuno <thinlam22@gmail.com>
2025-07-25 22:58:53 +0700

    chỉnh suu

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ app.post('/send-otp', async (req, res) => {
   const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',        // Đúng email
+    pass: 'abcd efgh ijkl mnop',        // Đúng app password 16 chữ
+  }
+});
+
 
   const mailOptions = {
     from: 'EFB App <' + process.env.EMAIL_USER + '>',
